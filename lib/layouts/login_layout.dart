0343ab488293e5da30b0165f9b981f9d48bfc426
PabloLasheras <plm0020@alu.medac.es>
2026-03-27 00:57:15 +0100

    añado blur

--- lib/layouts/login_layout.dart
+++ lib/layouts/login_layout.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:login_test/components/input_fields.dart';
 import 'package:login_test/core/app_colors.dart';
@@ -13,60 +14,73 @@ class _LoginLayoutState extends State<LoginLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(height: 70),
-                Text('EmpreGym', style: AppColors.brand),
-                Text(
-                  'Optimiza tu cuerpo, no sólo tu código.',
-                  style: AppColors.h1,
-                ),
-                const SizedBox(height: 150),
-                const InputFields(),
-                const SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'He olvidado mi contraseña',
-                    style: TextStyle(
-                      fontSize: 10,
-                      decoration: TextDecoration.underline,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.secondary,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 60,
-                  width: 200,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-                      backgroundColor: AppColors.secondary,
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Iniciar sesión',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          // Imagen de fondo
+          SizedBox.expand(
+            child: Image.asset(
+              "assets/images/background.jpg",
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+          // Filtro de desenfoque
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3), // Ajusta el blur
+              child: Container(
+                color: Colors.black.withAlpha((0.1 * 255).round()),
+              ),
+            ),
+          ),
+          // Contenido principal
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 70),
+                  Text('EmpreGym', style: AppColors.brand),
+                  Text(
+                    'Optimiza tu cuerpo, no sólo tu código.',
+                    style: AppColors.h1,
+                  ),
+                  const SizedBox(height: 150),
+                  const InputFields(),
+                  const SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'He olvidado mi contraseña',
+                      style: TextStyle(
+                        fontSize: 10,
+                        decoration: TextDecoration.underline,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.secondary,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 60,
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        backgroundColor: AppColors.secondary,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Iniciar sesión',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
